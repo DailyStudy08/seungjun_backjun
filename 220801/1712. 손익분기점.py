02558232e6c7b143db34d03dbbc02d970377642b
tmdwns950428 <tmdwns950428@naver.com>
2022-08-02 00:58:11 +0900

    220802

--- 220801/1712. 손익분기점.py
+++ 220801/1712. 손익분기점.py
@@ -1,16 +1,6 @@
 import sys
 A, B, C = map(int, sys.stdin.readline().split())
-cnt = 1
-while True:
-    if B-C >= 0:
-        print(-1)
-        break
-    else:
-        if B - C == -1:
-            print(A + 1)
-            break
-        elif A+(B-C)*cnt < 0:
-            print(cnt)
-            break
-        else:
-            cnt += 1
+if B>=C:
+    print(-1)
+else:
+    print(A//(C-B)+1)
