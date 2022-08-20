be01fe4c47f033277a38b5074fce9792debee5c2
tmdwns950428 <tmdwns950428@naver.com>
2022-08-20 20:01:47 +0900

    220820

--- 220817/N과 M(1).py
+++ 220817/N과 M(1).py
@@ -1,19 +1,15 @@
 n, m = map(int, input().split())
-
-visited = [0]*(n+1)
 answer = []
 
-def dfs(a):
-    if a == 0:
+def dfs(idx):
+    if idx == m:
         print(' '.join(map(str, answer)))
         return
 
     for i in range(1, n+1):
-        if not visited[i]:
+        if i not in answer:
             answer.append(i)
-            visited[i] = 1
-            dfs(a-1)
-            visited[i] = 0
+            dfs(idx+1)
             answer.pop()
 
-dfs(m)
+dfs(0)
