8129c47eb17f8a1bcff2330f9332424cde53224d
tmdwns950428 <tmdwns950428@naver.com>
2022-08-31 13:11:46 +0900

    220831

--- 220826/테트로미노.py
+++ 220826/테트로미노.py
@@ -1,45 +1,49 @@
 import sys
 sys.stdin = open('input.txt','r')
 
-def fucku(i, j):
+def fucku(r, c):
+    fuck_lst = []
+    global N, M, answer
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    for i in range(4):
+        nr, nc = r+dr[i], c+dc[i]
+        if 0 <= nr < N and 0 <= nc < M:
+            fuck_lst.append(arr[nr][nc])
 
-    total = []
-    for i in range(1, N-1):
-        for j in range(1, N-1):
-            lst = []
-            # 십자가 모양
-            lst.append(arr[i][j])
-            lst.append(arr[i][j-1])
-            lst.append(arr[i][j+1])
-            lst.append(arr[i-1][j])
-            lst.append(arr[i+1][j])
+    if len(fuck_lst) >= 3:
+        sumV = arr[r][c] + sum(sorted(fuck_lst, reverse=True)[:3])
+        answer = max(sumV, answer)
 
-            # 십자가에서 작은거 빼기
-            lst.remove(min(lst))
+def dfs(r, c, step, total):
+    global N, M, answer
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
 
-            # 4개 합을 total에 넣기
-            total.append(sum(lst))
+    if step == 4:
+        answer = max(answer, total)
+        return
 
-    return max(total)
+    for i in range(4):
+        nr, nc = r+dr[i], c+dc[i]
+        if 0 <= nr < N and 0 <= nc < M:
+            if not visited[nr][nc]:
+                visited[nr][nc] = 1
+                dfs(nr, nc, step+1, total+arr[nr][nc])
+                visited[nr][nc] = 0
 
-def dfs(i, j):
-    global mx_lst
-    stack = []
-    stack.append((i, j))
-
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, 1, 1]
-
-
-
+######################################################################
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-sr, sc = 1, 1
-#fuck you
-result = fucku(sr, sc)
+visited = [[0] * M for _ in range(N)]
+answer = 0
 
-mx_lst = []
 for i in range(N):
     for j in range(M):
-        dfs(i, j)
+        visited[i][j] = 1
+        dfs(i, j, 1, arr[i][j])
+        visited[i][j] = 0
+        fucku(i, j)
+
+print(answer)
