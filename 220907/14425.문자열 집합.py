c77e9c7869fb9c4d3317d010996c8a2eee1ce5f7
tmdwns950428 <tmdwns950428@naver.com>
2022-09-07 23:23:19 +0900

    220907

--- 220907/14425.문자열 집합.py
+++ 220907/14425.문자열 집합.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 # sys.stdin = open('input.txt', 'r')
 
 N, M = map(int, input().split())
-
 set_s = set(input() for _ in range(N))
-check = list(input() for _ in range(M))
 
 cnt = 0
-for i in set_s:
-    for j in check:
-        if i == j:
-            cnt += 1
+for _ in range(M):
+    j = input()
+    if j in set_s:
+        cnt += 1
 print(cnt)
